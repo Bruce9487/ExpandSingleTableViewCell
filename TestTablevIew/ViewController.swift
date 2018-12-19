daab6ca8c92759ffbe1ca41929cbe7ec97f3c1dc
APP技術部-陳冠志 <bruce@mitake.com.tw>
2018-12-19 09:40:50 +0800

    Initial Commit

---
+++ TestTablevIew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestTablevIew
+//
+//  Created by Bruce Chen on 2018/12/19.
+//  Copyright © 2018 APP技術部-陳冠志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
